e7eae01ca76e8d2ce4f9e2ddd452864e38cebf92
Andrei <andreilapusneanu2001@gmail.com>
2022-06-10 12:28:04 +0300

    changes

--- src/views/app-views/problemPage/index.js
+++ src/views/app-views/problemPage/index.js
@@ -1,101 +1,100 @@
-import React, {useEffect, useState} from 'react';
-import './problemPage.css';
-import { Input, Button, Table, Card, Form } from 'antd';
-import {
-    PlusOutlined
-} from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
+import "./problemPage.css";
+import { Input, Button, Table, Card, Form } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import axios from "axios";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
 const { TextArea } = Input;
 
 const ProblemPage = (props) => {
-    const { id } = useParams();
-    const history = useHistory();
-    const [form] = Form.useForm();
-    const [problemInfo, setProblemInfo] = useState({});
+  const { id } = useParams();
+  const history = useHistory();
+  const [form] = Form.useForm();
+  const [problemInfo, setProblemInfo] = useState({});
 
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8222/problems/` + id)
+      .then((res) => {
+        setProblemInfo(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        axios.get(`http://localhost:8222/problems/` + id).then((res) =>{
-            setProblemInfo(res.data)
-        }).catch(err => console.log(err))
-    }, [])
-
-    const submitSolution = (values) => {
-        const payload = {
-            "id_problem": id,
-            "id_user": props.token.id_user,
-            "solution": values.solution
-        }
-        axios
-            .post('https://URL/send-solution', payload)
-            .then(res => {
-                if (res.data.success == 1) {
-                    history.push("/app/problems")
-                }
-            });
+  const submitSolution = (values) => {
+    const payload = {
+      problemId: parseInt(id, 10),
+      userId: props.token.id,
+      solution: values.solution,
     };
-
-    return (
-        <div className="container">
-            {Object.keys(problemInfo).length > 0 &&
-            <Card>
-                <div className="d-md-flex justify-content-md-between">
-                    <div>
-                        <div class="problem-title mt-3">{problemInfo.name}</div>
-                        <div>
-                            <p>
-
-                                <span>Upload date: <b>{problemInfo.dateAdded}</b></span><br />
-
-                            </p>
-                        </div>
-                    </div>
-                    <div className="mt-3 text-right">
-                        <h2 className="mb-1 font-weight-bold">Problem #{problemInfo.id}</h2>
-                        <p>Difficulty: <b style={{textTransform: 'capitalize'}}>{problemInfo.difficulty}</b></p>
-
-                    </div>
-                </div>
-                <p class="problem-description">{problemInfo.description}</p>
-                { props?.token?.role == "student" &&
-                <Form form={form} name="control-hooks" onFinish={submitSolution}>
-                    <Form.Item name="solution" rules={[{ required: true }]}>
-                        <TextArea rows={7} placeholder="Upload problem solution in order to be reviewed by the teacher" />
-                    </Form.Item>
-                    <Form.Item style={{ float: 'right' }}>
-                        <Button icon={<PlusOutlined />} type="primary" htmlType="submit" shape="round">
-                            Upload solution
-                        </Button>
-                    </Form.Item>
-                </Form>}
-            </Card> }
-        </div>
-    )
-}
-
-const mapStateToProps = ({ auth }) => {
-    const { token } =  auth;
-    return { token}
+    axios.post("https://URL/send-solution", payload).then((res) => {
+      if (res.data.success == 1) {
+        history.push("/app/problems");
+      }
+    });
   };
 
-  export default connect(mapStateToProps)(ProblemPage)
+  return (
+    <div className="container">
+      {Object.keys(problemInfo).length > 0 && (
+        <Card>
+          <div className="d-md-flex justify-content-md-between">
+            <div>
+              <div class="problem-title mt-3">{problemInfo.name}</div>
+              <div>
+                <p>
+                  <span>
+                    Upload date: <b>{problemInfo.dateAdded}</b>
+                  </span>
+                  <br />
+                </p>
+              </div>
+            </div>
+            <div className="mt-3 text-right">
+              <h2 className="mb-1 font-weight-bold">
+                Problem #{problemInfo.id}
+              </h2>
+              <p>
+                Difficulty:{" "}
+                <b style={{ textTransform: "capitalize" }}>
+                  {problemInfo.difficulty}
+                </b>
+              </p>
+            </div>
+          </div>
+          <p class="problem-description">{problemInfo.description}</p>
+          {props?.token?.role == "student" && (
+            <Form form={form} name="control-hooks" onFinish={submitSolution}>
+              <Form.Item name="solution" rules={[{ required: true }]}>
+                <TextArea
+                  rows={7}
+                  placeholder="Upload problem solution in order to be reviewed by the teacher"
+                />
+              </Form.Item>
+              <Form.Item style={{ float: "right" }}>
+                <Button
+                  icon={<PlusOutlined />}
+                  type="primary"
+                  htmlType="submit"
+                  shape="round"
+                  disabled={props.token.id == problemInfo.teacherId}
+                >
+                  Upload solution
+                </Button>
+              </Form.Item>
+            </Form>
+          )}
+        </Card>
+      )}
+    </div>
+  );
+};
 
+const mapStateToProps = ({ auth }) => {
+  const { token } = auth;
+  return { token };
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default connect(mapStateToProps)(ProblemPage);
--- src/views/app-views/problemsList/index.js
+++ src/views/app-views/problemsList/index.js
@@ -1,153 +1,160 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col, Card, Menu, Progress, Tooltip, Button } from 'antd';
-import Flex from 'components/shared-components/Flex';
-import './problemsList.css';
+import React, { useState, useEffect } from "react";
+import { Row, Col, Card, Menu, Progress, Tooltip, Button } from "antd";
+import Flex from "components/shared-components/Flex";
+import "./problemsList.css";
 import {
-    DeleteOutlined,
-    CheckCircleOutlined,
-    CalendarOutlined,
-    EyeOutlined
-} from '@ant-design/icons';
-import EllipsisDropdown from 'components/shared-components/EllipsisDropdown';
-import { COLORS } from 'constants/ChartConstant';
+  DeleteOutlined,
+  CheckCircleOutlined,
+  CalendarOutlined,
+  EyeOutlined,
+} from "@ant-design/icons";
+import EllipsisDropdown from "components/shared-components/EllipsisDropdown";
+import { COLORS } from "constants/ChartConstant";
 import axios from "axios";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 const ProblemsList = (props) => {
-    const [problems, setProblems] = useState([]);
-    const history = useHistory();
+  const [problems, setProblems] = useState([]);
+  const history = useHistory();
 
-    useEffect(() => {
-        axios.get(`http://localhost:8222/problems/`)
-            .then(res => {
-                console.log(res.data)
-                setProblems(res.data);
-            })
-    }, [])
+  useEffect(() => {
+    axios.get(`http://localhost:8222/problems/`).then((res) => {
+      setProblems(res.data);
+    });
+  }, []);
 
+  const ItemHeader = ({ name, category }) => (
+    <div>
+      <h4 className="mb-0">{name}</h4>
+      <span className="text-muted" style={{ textTransform: "capitalize" }}>
+        Difficulty: {category}
+      </span>
+    </div>
+  );
 
-    const ItemHeader = ({ name, category }) => (
-        <div>
-            <h4 className="mb-0">{name}</h4>
-            <span className="text-muted" style={{ textTransform: "capitalize" }}>Difficulty: {category}</span>
-        </div>
-    )
+  const ItemAction = ({ id, removeId }) => (
+    <EllipsisDropdown
+      menu={
+        <Menu>
+          <Menu.Item key="0" onClick={() => removeId(id)}>
+            <DeleteOutlined />
+            <span className="ml-2">Delete Problem</span>
+          </Menu.Item>
+        </Menu>
+      }
+    />
+  );
 
+  const ItemInfo = ({ totalSolved, totalCorrectSolved, dateAdded }) => (
+    <Flex alignItems="center">
+      <div className="mr-3">
+        <Tooltip title="Correct solutions">
+          <CheckCircleOutlined className="text-muted font-size-md" />
+          <span className="ml-1 text-muted">
+            {totalCorrectSolved}/{totalSolved}
+          </span>
+        </Tooltip>
+      </div>
+      <div className="mr-3">
+        <Tooltip title="Date uploaded">
+          <CalendarOutlined className="text-muted font-size-md" />
+          <span className="ml-1 text-muted">{dateAdded}</span>
+        </Tooltip>
+      </div>
+    </Flex>
+  );
 
-    const ItemAction = ({ id, removeId }) => (
-        <EllipsisDropdown
-            menu={
-                <Menu>
-                    <Menu.Item key="0" onClick={() => removeId(id)}>
-                        <DeleteOutlined />
-                        <span className="ml-2">Delete Problem</span>
-                    </Menu.Item>
-                </Menu>
-            }
-        />
-    )
+  const ItemProgress = ({ progression }) => (
+    <Progress
+      percent={progression}
+      strokeColor={getProgressStatusColor(progression)}
+      size="small"
+    />
+  );
 
-    const ItemInfo = ({ totalSolved, totalCorrectSolved, dateAdded }) => (
-        <Flex alignItems="center">
-            <div className="mr-3">
-                <Tooltip title="Correct solutions">
-                    <CheckCircleOutlined className="text-muted font-size-md" />
-                    <span className="ml-1 text-muted">{totalCorrectSolved}/{totalSolved}</span>
-                </Tooltip>
-            </div>
-            <div className="mr-3">
-                <Tooltip title="Date uploaded">
-                    <CalendarOutlined className="text-muted font-size-md" />
-                    <span className="ml-1 text-muted">{dateAdded}</span>
-                </Tooltip>
-            </div>
-        </Flex>
-    )
-
-    const ItemProgress = ({ progression }) => (
-        <Progress percent={progression} strokeColor={getProgressStatusColor(progression)} size="small" />
-    )
-
-    const getProgressStatusColor = progress => {
-        if (progress >= 80) {
-            return COLORS[1]
-        }
-        if (progress < 60 && progress > 30) {
-            return COLORS[3]
-        }
-        if (progress < 30) {
-            return COLORS[2]
-        }
-        return COLORS[0]
+  const getProgressStatusColor = (progress) => {
+    if (progress >= 80) {
+      return COLORS[1];
     }
-
-
-    const GridItem = ({ data, removeId }) => (
-        <Card>
-            <Flex alignItems="center" justifyContent="between">
-                <ItemHeader name={data.name} category={data.difficulty} />
-                {props?.token?.role == "student" && props?.token?.id == data.id_user &&
-                <ItemAction id={data.id} removeId={removeId} />}
-            </Flex>
-            <div className="mt-2">
-                <ItemInfo
-                    totalSolved={data.totalSolved}
-                    totalCorrectSolved={data.totalCorrect}
-                    dateAdded={data.dateAdded}
-                />
-            </div>
-            <div className="mt-3">
-                <ItemProgress progression={Math.round((data.totalCorrect / data.totalSolved) * 100)} />
-            </div>
-
-            <div className="mt-3 problem-button-row">
-                <Button icon={<EyeOutlined />} type="primary" htmlType="submit" shape="round" onClick={() => history.push(`/app/problem/${data.id}`)}>
-                    See details
-                </Button>
-            </div>
-        </Card>
-    )
-
-    const deleteProblem = id => {
-        const payload = {
-            id_user: props.token.id,
-            id_problem: id
-        };
-        axios
-            .post('https://URL/delete-problem', payload)
-            .then(res => {
-                if (res.data.success == 1) {
-                    const data = problems.filter(elm => elm.id !== id)
-                    setProblems(data)
-                }
-            });
+    if (progress < 60 && progress > 30) {
+      return COLORS[3];
     }
-
-    return (
-        <div class="problems-list-container">
-            <div class="problems-list-title">Problems list</div>
-            <div className="my-4 container-fluid">
-                {
-                    <Row gutter={16}>
-                        {problems.map(elm => (
-                            <Col xs={24} sm={24} lg={8} xl={8} xxl={6} key={elm.id}>
-                                <GridItem
-                                    data={elm}
-                                    removeId={id => deleteProblem(id)}
-                                />
-                            </Col>
-                        ))}
-                    </Row>
-                }
-            </div>
-        </div>
-    )
-}
-
-const mapStateToProps = ({ auth }) => {
-    const { token } =  auth;
-    return { token}
+    if (progress < 30) {
+      return COLORS[2];
+    }
+    return COLORS[0];
   };
 
-  export default connect(mapStateToProps)(ProblemsList)
+  const GridItem = ({ data, removeId }) => (
+    <Card>
+      <Flex alignItems="center" justifyContent="between">
+        <ItemHeader name={data.name} category={data.difficulty} />
+        {props?.token?.id == data.teacherId && (
+          <ItemAction id={data.id} removeId={removeId} />
+        )}
+      </Flex>
+      <div className="mt-2">
+        <ItemInfo
+          totalSolved={data.totalSolved}
+          totalCorrectSolved={data.totalCorrect}
+          dateAdded={data.dateAdded}
+        />
+      </div>
+      <div className="mt-3">
+        <ItemProgress
+          progression={Math.round((data.totalCorrect / data.totalSolved) * 100)}
+        />
+      </div>
+
+      <div className="mt-3 problem-button-row">
+        <Button
+          icon={<EyeOutlined />}
+          type="primary"
+          htmlType="submit"
+          shape="round"
+          onClick={() => history.push(`/app/problem/${data.id}`)}
+        >
+          See details
+        </Button>
+      </div>
+    </Card>
+  );
+
+  const deleteProblem = (id) => {
+    const payload = {
+      id_user: props.token.id,
+      id_problem: id,
+    };
+    axios.post("https://URL/delete-problem", payload).then((res) => {
+      if (res.data.success == 1) {
+        const data = problems.filter((elm) => elm.id !== id);
+        setProblems(data);
+      }
+    });
+  };
+
+  return (
+    <div class="problems-list-container">
+      <div class="problems-list-title">Problems list</div>
+      <div className="my-4 container-fluid">
+        {
+          <Row gutter={16}>
+            {problems.map((elm) => (
+              <Col xs={24} sm={24} lg={8} xl={8} xxl={6} key={elm.id}>
+                <GridItem data={elm} removeId={(id) => deleteProblem(id)} />
+              </Col>
+            ))}
+          </Row>
+        }
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ auth }) => {
+  const { token } = auth;
+  return { token };
+};
+
+export default connect(mapStateToProps)(ProblemsList);
